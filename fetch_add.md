6b3d739de06596cf76241685d2f3a5110b4d2756
Akira Takahashi <faithandbrave@gmail.com>
2013-03-11 19:17:32 +0900

    テーブルに対応

--- fetch_add.md
+++ fetch_add.md
@@ -3,25 +3,36 @@ T fetch_add(T operand, [memory_order](https://sites.google.com/site/cpprefjp/ref
 T fetch_add(T operand, [memory_order](https://sites.google.com/site/cpprefjp/reference/atomic/memory_order) order = [memory_order_seq_cst](https://sites.google.com/site/cpprefjp/reference/atomic/memory_order)) noexcept;
 ```
 ##概要
+
 <b>加算を行う</b>
 
+
 ##効果
+
 <code>order</code>で指定されたメモリオーダーにしたがって、現在の値に<code>operandを加算した値</code>でアトミックに置き換える
 
 
+
 ##戻り値
+
 変更前の値が返される
 
 
+
 ##例外
+
 投げない
 
+
 ##備考
+
 この関数は、<code>atomic</code>クラスの整数型およびポインタに対する特殊化で定義される。
 
 符号付き整数型に対しては、2の補数表現による演算が行われ、未定義動作はない。アドレス型に関しては結果として未定義アドレスになる場合があるが、それ以外の未定義動作はない。
 
+
 ##例
+
 ```cpp
 #include <iostream>
 #include <atomic>
@@ -37,17 +48,23 @@ int main()
 }
 ```
 ###出力
+
 ```cpp
 3
 5
 ```
 ##バージョン
+
+
 ###言語
+
 
 - C++11
 
 
+
 ###処理系
+
 - [Clang](https://sites.google.com/site/cpprefjp/implementation#clang): ??
 - [GCC](https://sites.google.com/site/cpprefjp/implementation#gcc): 
 - [GCC, C++0x mode](https://sites.google.com/site/cpprefjp/implementation#gcc): 4.7.0
@@ -55,5 +72,7 @@ int main()
 - [Visual C++](https://sites.google.com/site/cpprefjp/implementation#visual_cpp) ??
 
 
+
 ##参照
+
 
--- html_to_markdown.rb
+++ html_to_markdown.rb
@@ -3,6 +3,89 @@
 
 require 'cgi'
 
+def getTdList(tds)
+  td_list = Array.new
+  tds.gsub(/<td(.*?)>(.*?)<\/td>/) {
+    td_list << $2
+  }
+  return td_list
+end
+
+def getTrList(trs)
+  tr_list = Array.new
+  trs.gsub(/<tr>(.*?)<\/tr>/) {
+    tr_list << $1
+  }
+
+  tr_list_s = Array.new
+  tr_list.each {|tds|
+    tr_list_s << getTdList(tds)
+  }
+
+  return tr_list_s
+end
+
+def getColumnWidthList(tr_list)
+  col_size = tr_list.first.size
+  max_col_width = 44
+
+  col_width_list = Array.new
+  col_size.times {|col|
+    col_width_list << (tr_list.transpose[col].max {|a, b| a.length <=> b.length }).length
+  }
+  return col_width_list
+end
+
+def makeTableHead(head, col_width_list)
+  result = "|"
+
+  head.each {|data|
+    result = result + data + " |"
+  }
+  result = result + "\n|"
+
+  col_width_list.each {|width|
+    line = "-" * width
+    result = result + line + "|"
+  }
+
+  return result
+end
+
+def makeTableBody(body, col_width_list)
+  result = ""
+  body.each {|row|
+    if !result.empty?
+      result = result + "\n"
+    end
+    result = result + "|"
+
+    row.each {|data|
+      result = result + data + " |"
+    }
+  }
+  return result
+end
+
+# テーブルの解析
+def parseTable(html)
+  parse_result = html.gsub(/<table(.*?)><tbody>(.*?)<\/tbody><\/table>/){|matched|
+    table = matched # <tr><td>...</td></tr>...</tr>
+    tr_list = getTrList(table)
+    col_width_list = getColumnWidthList(tr_list)
+
+    head = tr_list.first
+    body = tr_list[1, tr_list.size - 1]
+
+    result = ""
+    result = makeTableHead(head, col_width_list)
+    result = result + "\n" + makeTableBody(body, col_width_list) + "\n"
+
+    result
+  }
+  return parse_result
+end
+
 def htmlToMarkdown(html_path)
   filename = File.basename(html_path, ".html")
 
@@ -27,6 +110,7 @@ def htmlToMarkdown(html_path)
   # ゴミ消し
   html = html.gsub('<font face=\'monospace\' color=\'#000000\'>', "")
   html = html.gsub('<font color=\'#000000\'>', "")
+  html = html.gsub('<a name=(.*?)\/>', "")
 
   html = html.gsub("<br/>", "")
   html = html.gsub(/<a name=(.*?)>/, "")
@@ -36,9 +120,13 @@ def htmlToMarkdown(html_path)
   html = html.gsub(/<font color=\'#000000\'>(.*?)<\/font>/, '\1') # 意味がないので無視
   html = html.gsub('</font>', '') # 消し残し
 
+  # テーブル
+  html = parseTable(html)
+
   # 見出し
-  html = html.gsub(/<h2>(.*?)<\/h2>/, '##\1')
-  html = html.gsub(/<h3>(.*?)<\/h3>/, '###\1')
+  html = html.gsub(/<h2(.*?)>(.*?)<\/h2>/, "\n\n##\\2\n")
+  html = html.gsub(/<h3><\/h3>/, '')
+  html = html.gsub(/<h3(.*?)>(.*?)<\/h3>/, "\n\n###\\2\n")
 
   # 箇条書き
   html = html.gsub(/<li>(.*?)<\/li>/, "\n- \\1")
@@ -46,7 +134,7 @@ def htmlToMarkdown(html_path)
   html = html.gsub('</ul>', "")
 
   # リンク
-  html = html.gsub(/<a href=\'(.*?)\'>(.*?)<\/a>/, '[\2](\1)')
+  html = html.gsub(/<a(.*?) href=\'(.*?)\'>(.*?)<\/a>/, '[\3](\2)')
 
   # コードブロックの開始
   html = html.gsub('<div class=\'sites-codeblock sites-codesnippet-block\'>', '<codeblock>')
@@ -59,10 +147,15 @@ def htmlToMarkdown(html_path)
 
   # インデントによるコードブロック
 #  html = html.gsub(/<codeblock>(.*?)<\/codeblock>/m) {|codeblock|
-#	  codeblock = codeblock.gsub(/<codeblock>\n/m, "")
-#	  codeblock = codeblock.gsub(/\n<\/codeblock>/m, "")
-#	  codeblock.gsub(/^/, '    \1')
+#     codeblock = codeblock.gsub(/<codeblock>\n/m, "")
+#     codeblock = codeblock.gsub(/\n<\/codeblock>/m, "")
+#     codeblock.gsub(/^/, '    \1')
 #  }
+
+  # コードブロック内のゴミ消し
+  html = html.gsub(/<codeblock>(.*?)<\/codeblock>/m) {|codeblock|
+    codeblock = codeblock.gsub('<code>', '').gsub('</code>', '')
+  }
 
   # バッククォートでコードブロック
   html = html.gsub(/<codeblock>/, '```cpp')
@@ -76,6 +169,15 @@ def htmlToMarkdown(html_path)
 
   # スペースのような何かをスペースに置き換え
   html = html.gsub(' ', ' ')
+
+  # ゴミ消し
+  html = html.gsub(/<div(.*?)>/, "")
+  html = html.gsub("<div/>", "")
+  html = html.gsub("<h2>", "")
+  html = html.gsub("</h2>", "")
+  html = html.gsub(/<font(.*?)>(.*?)<\/font>/, '\2')
+  html = html.gsub(/<font(.*?)>/, '')
+  html = html.gsub(/<span(.*?)>(.*?)<\/span>/, '\2')
 
   File.open("#{filename}.md", "w") {|f|
     f.write(html)
